41899b22860dda702f42c6be89f9289da2345dd0
Shameer Rose <roseshammy@gmail.com>
2025-05-24 18:55:30 -0400

    Basic layout/styling for main page
    * Add basic html layout for navbar
    
    * Add basic navbar styling
    
    * Add account action layout, complete basic navbar styling
    
    * Add search bar layout/styling
    
    * Set navbar container height to 60px
    
    * Add basic grocery list layout/styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-export default function App() {
+import './index.css'
+import placeholder from './imgs/placeholder.png'
+
+function Navbar() {
+  return (
+  <>
+  <div className='navbar-container'> 
+
+    <div className='logo'>Prep'd</div> {/* Placeholder for logo */}
+
+    <ul className='navbar-list'>
+      <li>Add Groceries</li>
+      <li>Find Meals</li>
+      <li>Saved Meals</li>
+    </ul>
+
+    <div className='account-actions'>
+       <span>🔔</span>
+       <span>👤</span>
+    </div>
+    
+  </div>
+  </>
+  ) 
+}
+
+function SearchBar() {
+  return (
+    <div className='search-container'>
+    
+    <input placeholder='Add groceries...'></input>
+    
+    </div>
+  )
+}
+
+function GroceryCard(prop) {
   return (
     <>
-    <div>
-      <h1>
-        Welcome to Prep'd
-      </h1>
+    <div className='grocery-card'>
+      <img src={placeholder}></img>
+      <p>{prop.grocery_name} {prop.quantity}x</p>
     </div>
     </>
+  )
+}
+
+export default function App() {
+  return (
+    <div className='main-container'>
+      <Navbar></Navbar>
+      <SearchBar></SearchBar>
+      <h1>Grocery List:</h1>
+      <div className='grocery-container'>
+       <GroceryCard grocery_name="Apple" quantity="2"></GroceryCard>
+       <GroceryCard grocery_name="Banana" quantity="4"></GroceryCard>
+      </div>
+    </div>
   );
 }
