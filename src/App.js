0062e646d12da34c12ce692a36a0572b43057ff9
Shameer-R <roseshammy@gmail.com>
2025-05-24 14:57:23 -0700

    Add basic navbar styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ function Navbar() {
   return (
   <>
   <div className='navbar-container'> 
-    <li>Prep'd</li> {/* Placeholder for logo */}
+
+    <div className='logo'>Prep'd</div> {/* Placeholder for logo */}
+
     <ul className='navbar-list'>
       <li>Add Groceries</li>
       <li>Find Meals</li>
       <li>Saved Meals</li>
     </ul>
+
+    
   </div>
   </>
   ) 
