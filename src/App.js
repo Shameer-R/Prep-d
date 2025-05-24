752a826b4a62e9e419f87c7d14a76882f0ebf494
Shameer-R <roseshammy@gmail.com>
2025-05-24 14:47:10 -0700

    Add basic html layout for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import './index.css'
+
+function Navbar() {
+  return (
+  <>
+  <div className='navbar-container'> 
+    <li>Prep'd</li> {/* Placeholder for logo */}
+    <ul className='navbar-list'>
+      <li>Add Groceries</li>
+      <li>Find Meals</li>
+      <li>Saved Meals</li>
+    </ul>
+  </div>
+  </>
+  ) 
+}
+
 export default function App() {
   return (
     <>
-    <div>
-      <h1>
-        Welcome to Prep'd
-      </h1>
-    </div>
+    <Navbar></Navbar>
     </>
   );
 }
